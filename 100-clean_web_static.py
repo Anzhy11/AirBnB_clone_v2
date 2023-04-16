f294246c61b4e071aaf4412eea551bbee83a853f
Anzhy11 <harnzzy@gmail.com>
2023-04-16 16:04:22 +0100

    added server

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -6,7 +6,7 @@ that deletes out-of-date archives, using the function do_clean
 import os
 from fabric.api import cd, env, local, run
 
-env.hosts = ["34.231.110.206", "3.239.57.196"]
+env.hosts = ["52.3.243.73", "52.87.220.53"]
 
 
 def do_clean(number=0):
